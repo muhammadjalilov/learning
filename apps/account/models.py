f00157714a644a1c160d97b8126fb4bbcb57503f
Asus <shuhratabduraxmonov08@gmail.com>
2024-09-26 18:22:27 +0500

    added Account model

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from apps.shared.models import BaseModel
+
+
+class Account(AbstractUser, BaseModel):
+    website = models.CharField(max_length=128)
+    avatar = models.ImageField(upload_to='avatars/')
+
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.username
+
