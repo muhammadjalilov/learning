6bb0b40ee4225a961f3ba0ed8a46fb29ada284c9
muhammadjalilov <mjalilov2004@gmail.com>
2024-09-27 22:17:14 +0500

    register account model

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-# Register your models here.
+from apps.account.models import Account
+from apps.instructors.models import Instructor
+from apps.students.models import Student
+
+
+class InstructorInline(TabularInline):
+    model = Instructor
+    extra = 1
+
+
+class StudentInline(TabularInline):
+    model = Student
+    extra = 1
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    inlines = [InstructorInline, StudentInline]
