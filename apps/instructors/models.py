4dd4b689ced903b7f4d5cceffd093de5d3d870b0
muhammadjalilov <mjalilov2004@gmail.com>
2024-09-27 22:24:03 +0500

    changed student and instructor

--- apps/instructors/models.py
+++ apps/instructors/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 from apps.account.models import Account
+from apps.shared.models import TimeStampedModel
 
 
-class Instructor(Account):
+class Instructor(TimeStampedModel):
+    account = models.OneToOneField('account.Account',on_delete=models.CASCADE)
     class Meta:
         verbose_name = 'Instructor'
         verbose_name_plural = 'Instructors'
